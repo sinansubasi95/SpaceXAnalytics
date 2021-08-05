98d0efed1c004d3300f30e3b8b1087f75aa3cf10
sinansubasi95 <sinan95@gmail.com>
2021-08-05 23:15:41 +0300

    add styled-components and twin.macro

--- src/components/atoms/Anchor/Anchor.tsx
+++ src/components/atoms/Anchor/Anchor.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
+import Link from 'next/link';
 
-interface IAnchorProps {}
+interface IAnchorProps {
+  className: string;
+  href: string;
+}
 
-const Anchor: React.FC<IAnchorProps> = ({ children }) => {
+const Anchor: React.FC<IAnchorProps> = ({ children, className, href }) => {
+  if (className) {
+    return (
+      <Link href={href}>
+        <a className={className}>{children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <a
-      className="
-        border-transparent
-        text-gray-600
-        hover:bg-gray-50
-        hover:text-gray-900
-        group
-        flex
-        items-center
-        px-3
-        py-2
-        text-sm
-        font-medium
-        border-r-4
-      "
-    >
-      {children}
-    </a>
+    <Link href={href}>
+      <a
+        className="
+          border-transparent
+          text-gray-600
+          hover:bg-gray-50
+          hover:text-gray-900
+          group
+          flex
+          items-center
+          px-3
+          py-2
+          text-sm
+          font-medium
+          border-r-4
+          active:bg-indigo-50
+          active:border-indigo-600
+          active:text-indigo-600
+        "
+      >
+        {children}
+      </a>
+    </Link>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import {
   InboxIcon,
   UsersIcon,
 } from '@heroicons/react/outline';
+import React from 'react';
+import Anchor from '../components/atoms/Anchor/Anchor';
 
 const navigation = [
   { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
@@ -35,15 +37,15 @@ export default function Example() {
           <div className="mt-5 flex-grow flex flex-col">
             <nav className="flex-1 bg-white space-y-1" aria-label="Sidebar">
               {navigation.map((item) => (
-                <a
+                <Anchor
                   key={item.name}
-                  href={item.href}
                   className={classNames(
                     item.current
                       ? 'bg-indigo-50 border-indigo-600 text-indigo-600'
                       : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
                     'group flex items-center px-3 py-2 text-sm font-medium border-r-4'
                   )}
+                  href="/cat"
                 >
                   <item.icon
                     className={classNames(
@@ -55,7 +57,7 @@ export default function Example() {
                     aria-hidden="true"
                   />
                   {item.name}
-                </a>
+                </Anchor>
               ))}
             </nav>
           </div>
