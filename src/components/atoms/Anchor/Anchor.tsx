c55d79eec86588293cd7632f6aa2942c64bda1e2
sinansubasi95 <sinan95@gmail.com>
2021-08-06 00:34:43 +0300

    migrate anchor component to styled-component

--- src/components/atoms/Anchor/Anchor.tsx
+++ src/components/atoms/Anchor/Anchor.tsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+// import tw from 'twin.macro';
 
 interface IAnchorProps {
-  className: string;
   href: string;
 }
 
-const Anchor: React.FC<IAnchorProps> = ({ children, className, href }) => {
-  if (className) {
+const BaseAnchor = styled.a`
+  color: red;
+  /* ${tw`
+    border-transparent
+    text-gray-600
+    hover:bg-gray-50
+    hover:text-gray-900
+    flex
+    items-center
+    px-3
+    py-2
+    text-sm
+    font-medium
+    border-r-4
+    active:bg-indigo-50
+    active:border-indigo-600
+    active:text-indigo-600
+  `} */
+`;
+
+const Anchor: React.FC<IAnchorProps> = ({ children, href }) => {
+  if (href) {
     return (
       <Link href={href}>
-        <a className={className}>{children}</a>
+        <BaseAnchor>{children}</BaseAnchor>
       </Link>
     );
   }
 
   return (
     <Link href={href}>
-      <a
-        className="
-          border-transparent
-          text-gray-600
-          hover:bg-gray-50
-          hover:text-gray-900
-          group
-          flex
-          items-center
-          px-3
-          py-2
-          text-sm
-          font-medium
-          border-r-4
-          active:bg-indigo-50
-          active:border-indigo-600
-          active:text-indigo-600
-        "
-      >
-        {children}
-      </a>
+      <BaseAnchor>{children}</BaseAnchor>
     </Link>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,16 +37,7 @@ export default function Example() {
           <div className="mt-5 flex-grow flex flex-col">
             <nav className="flex-1 bg-white space-y-1" aria-label="Sidebar">
               {navigation.map((item) => (
-                <Anchor
-                  key={item.name}
-                  className={classNames(
-                    item.current
-                      ? 'bg-indigo-50 border-indigo-600 text-indigo-600'
-                      : 'border-transparent text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                    'group flex items-center px-3 py-2 text-sm font-medium border-r-4'
-                  )}
-                  href="/cat"
-                >
+                <Anchor key={item.name} href="/cat">
                   <item.icon
                     className={classNames(
                       item.current
