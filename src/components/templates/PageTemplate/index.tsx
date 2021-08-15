2ff856dd3bd267cdc62c0756e38ce5abd9af55fd
sinansubasi95 <sinan95@gmail.com>
2021-08-15 23:59:43 +0300

    use bulletproof react architecture

--- src/components/templates/PageTemplate/index.tsx
+++ src/components/templates/PageTemplate/index.tsx
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React, { Fragment, useState } from 'react';
+import { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import {
   CalendarIcon,
@@ -9,12 +9,8 @@ import {
   InboxIcon,
   MenuIcon,
   UsersIcon,
+  XIcon,
 } from '@heroicons/react/outline';
-import Drawer from '../../organisms/drawer';
-import Sidebar from '../../organisms/sidebar';
-// import Navigation from '../components/molecules/navigation';
-
-import { PageContainer, Wrapper } from './style';
 
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
@@ -29,31 +25,164 @@ function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function PageTemplate() {
-  // use state management library
+type MobileSidebarProps = {
+  sidebarOpen: boolean;
+  setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MobileSidebar = ({ sidebarOpen, setSidebarOpen }: MobileSidebarProps) => {
+  return (
+    <Transition.Root show={sidebarOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        static
+        className="fixed inset-0 flex z-40 md:hidden"
+        open={sidebarOpen}
+        onClose={setSidebarOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="absolute top-0 right-0 -mr-12 pt-2">
+                <button
+                  className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  onClick={() => setSidebarOpen(false)}
+                >
+                  <span className="sr-only">Close sidebar</span>
+                  <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                </button>
+              </div>
+            </Transition.Child>
+            <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+              <div className="flex-shrink-0 flex items-center px-4">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <nav className="mt-5 px-2 space-y-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-100 text-gray-900'
+                        : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                      'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                    )}
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current
+                          ? 'text-gray-500'
+                          : 'text-gray-400 group-hover:text-gray-500',
+                        'mr-4 flex-shrink-0 h-6 w-6'
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </Transition.Child>
+        <div className="flex-shrink-0 w-14">
+          {/* Force sidebar to shrink to fit close icon */}
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <div className="hidden md:flex md:flex-shrink-0">
+      <div className="flex flex-col w-64">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                alt="Workflow"
+              />
+            </div>
+            <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-100 text-gray-900'
+                      : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
+                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                  )}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? 'text-gray-500'
+                        : 'text-gray-400 group-hover:text-gray-500',
+                      'mr-3 flex-shrink-0 h-6 w-6'
+                    )}
+                    aria-hidden="true"
+                  />
+                  {item.name}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  // const PageTemplate = ({
-  //   header, hero, sponsor, children, footer, ...props
-  // }) => {
-  //   return (
-  //     <Wrapper {...props}>
-  //       <Header>{header}</Header>
-  //       {hero && <Hero>{hero}</Hero>}
-  //       {sponsor && <Sponsor>{sponsor}</Sponsor>}
-  //       <Content>{children}</Content>
-  //       <Footer>{footer}</Footer>
-  //     </Wrapper>
-  //   )
-  // }
-
   return (
-    <Wrapper>
-      <Drawer />
+    <div className="h-screen flex overflow-hidden bg-gray-100">
+      <MobileSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+      {/* Static sidebar for desktop */}
       <Sidebar />
-      <PageContainer>
-        {/* Hamburger */}
-        {/* <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+      <div className="flex flex-col w-0 flex-1 overflow-hidden">
+        <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
           <button
             className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             onClick={() => setSidebarOpen(true)}
@@ -61,8 +190,7 @@ export default function PageTemplate() {
             <span className="sr-only">Open sidebar</span>
             <MenuIcon className="h-6 w-6" aria-hidden="true" />
           </button>
-        </div> */}
-        {/* Main Content */}
+        </div>
         <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
           <div className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
@@ -79,7 +207,7 @@ export default function PageTemplate() {
             </div>
           </div>
         </main>
-      </PageContainer>
-    </Wrapper>
+      </div>
+    </div>
   );
 }
