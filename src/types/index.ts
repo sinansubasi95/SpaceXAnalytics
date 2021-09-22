0d5d15242e5b8966b8f80154af8f17aa489b243c
sinansubasi95 <64268680+sinansubasi95@users.noreply.github.com>
2021-09-22 20:57:51 +0000

    update tasks

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,12 @@
 // somehow seperate api types and ui types
 // use interface instead of type?
 // learn what declare is and its advantages
+// references:
+// https://github.com/itchio/itch/tree/f88c47b9db45882872f3ad2d647c83b702ae4584/typings
+// https://github.com/railsware/upterm/tree/master/typings
+// https://github.com/tsedio/tsed/tree/production/packages/core
+// https://github.com/zvikfir/MyWay-client
+// https://www.typescriptlang.org/docs/handbook/2/generics.html
 
 export type LayoutT = {
     leftPanel: React.ReactElement;
