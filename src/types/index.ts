f879a78a782f462e7feebd9a3246c16b61ec3884
sinansubasi95 <64268680+sinansubasi95@users.noreply.github.com>
2021-09-24 20:54:17 +0000

    update tasks

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,7 +8,34 @@
 // https://github.com/tsedio/tsed/tree/production/packages/core
 // https://github.com/zvikfir/MyWay-client
 // https://www.typescriptlang.org/docs/handbook/2/generics.html
+// https://github.com/labs42io/clean-code-typescript#classes
+// https://blog.bitsrc.io/react-typescript-cheetsheet-2b6fa2cecfe2
+// https://www.youtube.com/watch?v=lSrpmK1nxow
+// https://www.youtube.com/watch?v=bM5iOUTTFoo
+// https://www.youtube.com/watch?v=vjKWPJTK5gc
+// https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-classes-structs-and-interfaces
+// https://stackoverflow.com/questions/681700/interface-naming-convention
+// https://stackoverflow.com/questions/35774620/typescript-style-guide-for-interfaces
+// https://stackoverflow.com/questions/48138111/what-typescript-type-should-i-use-to-reference-the-match-object-in-my-props
 
+// PropTypes: https://reactjs.org/docs/typechecking-with-proptypes.html#proptypes
+// prop-types is a lib
+
+/*
+    --- final decision ---
+
+*/
+
+
+/*
+- types / interfaces
+-- props
+--- ILayout? / LayoutProps? / ThreeColumnLayoutProps?
+--- INavigationList
+--- ICategorizedNavigationList
+--- ILogo
+-- 
+*/
 export type LayoutT = {
     leftPanel: React.ReactElement;
     children: React.ReactElement;
