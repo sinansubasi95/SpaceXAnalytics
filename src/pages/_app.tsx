aa0d5fab7d6980a5a6f48fd5196e4d5728e4f09c
sinansubasi95 <64268680+sinansubasi95@users.noreply.github.com>
2021-10-03 09:37:17 +0000

    add urql provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useStores } from '../stores';
+import { Provider } from 'urql';
+import { client, ssrCache } from '../utils/createUrqlClient';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Handle navigation on load
@@ -9,6 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   const { navigationStore } = useStores();
   navigationStore.activateLink(pathname)
 
-  return <Component {...pageProps} />;
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 export default MyApp;
