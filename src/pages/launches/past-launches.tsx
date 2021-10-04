8118c0e47f5700695a91d48eff3bf73f2f8128e6
sinansubasi95 <64268680+sinansubasi95@users.noreply.github.com>
2021-10-04 05:42:27 +0000

    create PastLaunchesQuery

--- src/pages/launches/past-launches.tsx
+++ src/pages/launches/past-launches.tsx
@@ -1,7 +1,53 @@
 import {ThreeColumnLayout} from '../../components/Layout/ThreeColumnLayout';
 import LaunchesPanel from '../../components/Panel/LaunchesPanel';
+import { useQuery } from 'urql';
+import { client, ssrCache } from '../../utils/createUrqlClient';
+
+// https://api.spacex.land/graphql/
+
+const PastLaunchesQuery = `
+
+  query launchesPast($limit: Int) {
+    launchesPast(limit: $limit) {
+      mission_name
+      launch_date_utc
+      rocket {
+        rocket_name
+      }
+      id
+      details
+    }
+  }
+
+`;
+
+// const PastLaunchesQuery = `
+// {
+//   launchesPast(limit: 1) {
+//     mission_name
+//     launch_date_utc
+//     rocket {
+//       rocket_name
+//     }
+//     id
+//     details
+//   }
+// }
+// `;
 
 export default function PastLaunches() {
+  const [result] = useQuery({
+    query: PastLaunchesQuery,
+    variables: { limit: 1 }
+  });
+
+  const {data, fetching, error} = result;
+
+  if (fetching) return "Fetching";
+  if (error) return "Error";
+
+  console.log(JSON.stringify(data, null, 2));
+
   return (
     <ThreeColumnLayout leftPanel={<LaunchesPanel/>}>
       <div>test</div>
